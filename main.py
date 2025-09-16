9d047eb17e586032e52f2fe6a3c0bb7484d44516
erafwork <23f2003509@ds.study.iitm.ac.in>
2025-09-16 21:28:07 +0530

    Update main.py to use arithmetic modules

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('Welcome to Arithemetic Project')
+
+from sum import add
+from difference import subtract
+from product import multiply
+from division import divide
+
+print("2 + 3 =", add(2, 3))
+print("7 - 4 =", subtract(7, 4))
+print("5 * 6 =", multiply(5, 6))
+print("10 / 2 =", divide(10, 2))
+print("10 / 0 =", divide(10, 0))
